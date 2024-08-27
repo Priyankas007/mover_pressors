b3f006a3545f97c5ba9faff2ba8b2c5d4b792031
Priyanka Shrestha <shrestp@sh02-ln03.stanford.edu>
2024-08-27 09:40:04 -0700

    Updated scripts

--- filter_patient_meds.py
+++ filter_patient_meds.py
@@ -1,17 +1,19 @@
-import torch
+#import torch
 import pandas as pd
 import tarfile
 import os
 import matplotlib.pyplot as plt
 
 # Get only patients that were on pressor medications
-patient_meds = pd.read_csv('EPIC_EMR/EMR/patient_medications.csv', low_memory=False)
+
+patient_meds = pd.read_csv('/scratch/users/shrestp/mover/EPIC_EMR/EPIC_EMR/EMR/patient_medications.csv', low_memory=False)
 pressors = ['dopamine', 'vasopressin', 'dobutamine', 'epinephrine', 'norepinephrine']
 pattern = '|'.join(pressors)
-pressors_df = patient_meds[patient_meds['MEDICATION_NM'].str.contains(pattern, case=False, na=False)]patient_meds = pd.read_csv('EPIC_EMR/EMR/patient_medications.csv', low_memory=False)
+pressors_df = patient_meds[patient_meds['MEDICATION_NM'].str.contains(pattern, case=False, na=False)]
+#patient_meds = pd.read_csv('EPIC_EMR/EMR/patient_medications.csv', low_memory=False)
 
 # Get patients that were admitted to the ICU
-patient_information = pd.read_csv('EPIC_EMR/EMR/patient_information.csv')
+patient_information = pd.read_csv('/scratch/users/shrestp/mover/EPIC_EMR/EPIC_EMR/EMR/patient_information.csv')
 pressors_icu_df = pd.merge(pressors_df, patient_information[['LOG_ID', 'ICU_ADMIN_FLAG', 'SURGERY_DATE']], on='LOG_ID', how='left')
 
 # Get patients that only started on pressors after their surgery
@@ -20,4 +22,4 @@ pressors_icu_df['START_DATE'] = pd.to_datetime(pressors_icu_df['START_DATE'])
 pressors_icu_df_filtered = pressors_icu_df[pressors_icu_df['SURGERY_DATE'] < pressors_icu_df['START_DATE']]
 pressors_icu_df_filtered = pressors_icu_df_filtered[pressors_icu_df_filtered['ICU_ADMIN_FLAG'] == 'Yes']
 
-pressors_icu_df_filtered.to_csv('patient_medications_pressors.csv', index=False)
+pressors_icu_df_filtered.to_csv('/scratch/users/shrestp/mover/EPIC_EMR/EPIC_EMR/EMR/patient_medications_pressors.csv', index=False)
